ed9ed026b1ebfad1d6005b07b0eeb42e43187ae2
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2014-07-27 11:24:42 +0200

    doxygen

--- hierarchy.js
+++ hierarchy.js
@@ -5,12 +5,10 @@ var hierarchy =
       [ "DHTHumiditySensor", "class_d_h_t_humidity_sensor.html", null ],
       [ "DHTTemperatureSensor", "class_d_h_t_temperature_sensor.html", null ],
       [ "DS18B20Sensor", "class_d_s18_b20_sensor.html", null ],
+      [ "MagnetSensor", "class_magnet_sensor.html", null ],
+      [ "SoilMoistureSensor", "class_soil_moisture_sensor.html", null ],
       [ "VoltmeterSensor", "class_voltmeter_sensor.html", null ]
     ] ],
     [ "MeasurementNode", "class_measurement_node.html", null ],
-    [ "NRF24Transceiver", "class_n_r_f24_transceiver.html", null ],
-    [ "OneWireSensor", null, [
-      [ "MagnetSensor", "class_magnet_sensor.html", null ],
-      [ "SoilMoistureSensor", "class_soil_moisture_sensor.html", null ]
-    ] ]
+    [ "NRF24Transceiver", "class_n_r_f24_transceiver.html", null ]
 ];
