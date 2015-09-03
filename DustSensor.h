c32ecc1c86fb239a6efbdb6df030d1412dd00496
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2015-09-03 11:31:21 +0200

    Seeed studio dust sensor added

---
+++ DustSensor.h
@@ -0,0 +1,66 @@
+#pragma once
+/*
+  AFrame - Arduino framework library for ASensor and AWind libraries
+  Copyright (C)2014 Andrei Degtiarev. All right reserved
+  
+  You can always find the latest version of the library at 
+  https://github.com/AndreiDegtiarev/ASensor
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the MIT license.
+  Please see the included documents for further information.
+*/
+///Dust sensor from seeedstudio. No any external libraries are neccesary
+///Details to member functions see ISensor class documentation
+class DustSensor : public ISensor
+{
+	int _port;
+	unsigned long _starttime;
+	unsigned long _sampletime_ms;
+	unsigned long _lowpulseoccupancy;
+	float _concentration;
+public:
+	DustSensor(int port,unsigned long sample_time_ms)
+	{
+		_port=port;
+		_sampletime_ms=sample_time_ms;
+		_lowpulseoccupancy=0;
+		_concentration=0;
+		pinMode(_port,INPUT);
+		_starttime = millis();//get the current time;
+	}
+	const __FlashStringHelper* Name()
+	{
+		return F("Dust");
+	}
+	float LowMeasurementLimit()
+	{
+		return 0;
+	}
+	float HighMeasurementLimit()
+	{
+		return 1024;
+	}
+	int Precission()
+	{
+		return 0;
+	}
+	bool Measure(float &data)
+	{
+		unsigned long duration = pulseIn(_port, LOW);
+		_lowpulseoccupancy += duration;
+		unsigned long cur_sample_time=millis()-_starttime;
+		if ((millis()-_starttime) > _sampletime_ms)//if the sampel time == 30s
+		{
+			if(_lowpulseoccupancy>0)
+			{
+				float ratio = _lowpulseoccupancy/(cur_sample_time*10.0);  // Integer percentage 0=>100
+				_concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+			}
+			_lowpulseoccupancy = 0;
+			_starttime = millis();
+		}
+		data=_concentration;
+		return true;
+	}
+};
