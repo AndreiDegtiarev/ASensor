b6325474d3bcda860ac0184982212a62918e5ec2
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2015-09-05 12:29:46 +0200

    MQ4 methane gas sensor implemented

---
+++ MQ4MethaneGasSensor.h
@@ -0,0 +1,45 @@
+#pragma once
+/*
+  AFrame - Arduino framework library for ASensor and AWind libraries
+  Copyright (C)2014 Andrei Degtiarev. All right reserved
+  
+  You can always find the latest version of the library at 
+  https://github.com/AndreiDegtiarev/ASensor
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the MIT license.
+  Please see the included documents for further information.
+*/
+///MQ4 methane gas sensor from sainsmart. No any external libraries are neccesary
+///Details to member functions see ISensor class documentation
+class MQ4MethaneGasSensor : public ISensor
+{
+	int _port;
+
+public:
+	MQ4MethaneGasSensor(int port)
+	{
+		_port=port;
+	}
+	const __FlashStringHelper* Name()
+	{
+		return F("MQ4MethaneGas");
+	}
+	float LowMeasurementLimit()
+	{
+		return 0;
+	}
+	float HighMeasurementLimit()
+	{
+		return 1024;
+	}
+	int Precission()
+	{
+		return 0;
+	}
+	bool Measure(float &data)
+	{
+		data=(int)analogRead(_port);
+		return true;
+	}
+};
