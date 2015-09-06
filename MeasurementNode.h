f8b283a6e198cb583b4c36c9d196b771a3804bef
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2015-09-06 16:22:36 +0200

    header update

--- MeasurementNode.h
+++ MeasurementNode.h
@@ -1,8 +1,8 @@
 #pragma once
 /*
-  AFrame - Arduino framework library for ASensor and AWind libraries
-  Copyright (C)2014 Andrei Degtiarev. All right reserved
-  
+  ASensor - Sensor library. Can be used standalone or in conjunction with AWind library
+  Copyright (C)2015 Andrei Degtiarev. All right reserved
+ 
   You can always find the latest version of the library at 
   https://github.com/AndreiDegtiarev/ASensor
 
