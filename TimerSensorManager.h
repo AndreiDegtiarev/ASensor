056e5870c60bced8a975929fb9b1930763a6d418
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2016-03-30 20:36:34 +0200

    Timer sensor is implemented

---
+++ TimerSensorManager.h
@@ -0,0 +1,93 @@
+#pragma once
+/*
+ASensor - Sensor library. Can be used standalone or in conjunction with AWind library
+Copyright (C)2016 Andrei Degtiarev. All right reserved
+
+You can always find the latest version of the library at
+https://github.com/AndreiDegtiarev/ASensor
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the MIT license.
+Please see the included documents for further information.
+*/
+
+///Timer class. Implement timer events.
+class TimerSensorManager : public SensorManager , public ISensor
+{
+	uint32_t _msStart;                          //!< time when timer was started
+	uint32_t _msInterval;                       //!< timer interval
+	bool _isAutoreset;                          //!< defines whether timer event will begenerated only once or continiously
+	bool _isEnabled;                            //!< defines whether timer is active
+	bool _counter;
+public:
+	TimerSensorManager():SensorManager(this,0, 1,0,false)
+	{
+		_isAutoreset = false;
+		_isEnabled = false;
+		_counter = 0;
+	}
+	const __FlashStringHelper* Name()
+	{
+		return F("TimerSensor");
+	}
+	float LowMeasurementLimit()
+	{
+		return 0;
+	}
+	float HighMeasurementLimit()
+	{
+		return MAXFLOAT;
+	}
+	int Precission()
+	{
+		return 0;
+	}
+	bool Measure(float &data)
+	{
+		if (_isEnabled && Elapsed() >= _msInterval)
+		{
+			_counter=!_counter; //force event message
+			if (_isAutoreset)
+				Enable();
+			else
+				Reset();
+		}
+
+		data = _counter;
+		return true;
+	}
+	///Set whether timer is in autoreset modus
+	void SetAutoReset(bool isAutoreset)
+	{
+		_isAutoreset = isAutoreset;
+	}
+	///Set timer interval in milliseconds
+	void SetInterval(uint32_t milliseconds)
+	{
+		_msInterval = milliseconds;
+	}
+	///Returns true if timer is active
+	bool IsEnabled()
+	{
+		return _isEnabled;
+	}
+	///Starts timer
+	void Enable()
+	{
+		_isEnabled = true;
+		_msStart = millis();
+	}
+	///Disabels timer
+	void Reset()
+	{
+		_isEnabled = false;
+	}
+	///Returns actual elapsed time
+	uint32_t Elapsed()
+	{
+		if (_isEnabled)
+			return millis() - _msStart;
+		else
+			return 0;
+	}
+};
